c96d893c862e400f06c29a1868fcab7cb2baf1b8
Amesul <samuelstroesser@icloud.com>
2024-09-18 20:00:26 +0200

    Modification du type d'une colonne

--- database/migrations/2024_08_30_123729_create_streamers_table.php
+++ database/migrations/2024_08_30_123729_create_streamers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
             $table->id();
             $table->string('login');
             $table->string('display_name');
-            $table->string('description');
+            $table->text('description');
             $table->string('profile_image_url');
             $table->string('slc_access_token')->nullable();
             $table->string('title')->nullable();
