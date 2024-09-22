475b1ee09f39eeeef6fc5e10ed2be9a5f63951a6
Amesul <samuelstroesser@icloud.com>
2024-09-22 15:59:35 +0200

    Ajout d'une bulle de texte pour afficher les commandes du bot par intermittence

--- resources/views/assets/bandeau.blade.php
+++ resources/views/assets/bandeau.blade.php
@@ -12,12 +12,19 @@
 </head>
 
 <body class="h-screen w-screen overflow-hidden bg-none">
-<main class="absolute bottom-0 left-0 flex h-28 w-full justify-between border-t-4 px-8 bg-gray-950 border-secondary">
+
+<section id="bot_command_container"
+         class="absolute bottom-24 left-20 flex translate-y-12 justify-center rounded-t-lg px-6 pt-2 pb-6 align-middle shadow-lg transition-all ease-in-out duration-[2s] bg-slate-600 min-w-28">
+    <p id="bot_command" class="font-bold text-stone-100 font-raleway"></p>
+</section>
+
+<main
+    class="absolute bottom-0 left-0 z-20 flex h-28 w-full justify-between border-t-4 px-8 shadow-lg bg-slate-950 border-secondary">
     <div class="flex h-28 items-center gap-8">
         <img class="h-20" src="{{ asset('/storage/images/logo_ETC-full-flat.png') }}">
         <!-- Cagnotte globale -->
         <div class="relative h-16 w-56">
-            <p class="absolute left-1/2 inline-block -translate-x-1/2 truncate bg-none px-1 text-2xl font-medium -top-[14px] bg-gray-950 text-primary font-staatliches">
+            <p class="absolute left-1/2 inline-block -translate-x-1/2 truncate bg-none px-1 text-2xl font-medium -top-[14px] bg-slate-950 text-primary font-staatliches">
                 Cagnotte globale</p>
             <div
                 class="inline-flex h-full w-full place-items-center justify-center truncate rounded-md border-0 text-3xl text-white shadow-sm ring-2 ring-inset py-1.5 font-staatliches ring-primary">
@@ -27,7 +34,7 @@
         @if($cagnotte_perso)
             <!-- Cagnotte perso -->
             <div class="relative h-16 w-56">
-                <p class="absolute left-1/2 inline-block -translate-x-1/2 truncate bg-none px-1 text-2xl font-medium -top-[14px] bg-gray-950 text-primary font-staatliches">
+                <p class="absolute left-1/2 inline-block -translate-x-1/2 truncate bg-none px-1 text-2xl font-medium -top-[14px] bg-slate-950 text-primary font-staatliches">
                     Cagnotte perso</p>
                 <div
                     class="inline-flex h-full w-full place-items-center justify-center truncate rounded-md border-0 text-3xl text-stone-100 shadow-sm ring-2 ring-inset py-1.5 font-staatliches ring-primary">
@@ -45,15 +52,43 @@
         {{-- <img class="hidden h-20" src="{{ asset('/storage/images/logo_resonances-full.png') }}"> --}}
         <img class="h-20" src="{{ asset('/storage/images/logo_mixtape-records.png') }}">
         {{-- img class="hidden h-20" src="{{ asset('/storage/images/logo_mixtape-records-full.png') }}"> --}}
-        <div class="relative -ml-4 h-20 w-56">
-            <img class="absolute -bottom-24 rotate-6 -skew-x-3 z-10 brightness-110 contrast-125"
-                 src="{{ asset('storage/images/tshirt_V1.png') }}" alt="T-shirt">
+        <div class="relative -ml-2 h-20 w-56">
+            <img src="{{ asset('storage/images/tshirt_V1.png') }}"
+                 class="absolute -bottom-24 rotate-6 -skew-x-3 z-50 drop-shadow-[0px_2px_15px_rgba(245,245,244,0.18)] brightness-110 contrast-125">
         </div>
     </div>
 </main>
 
 <script>
+    // Durée d'affichage de la bulle de texte, en secondes (3s minimum)
+    const duration = 8;
+    // Intervalle entre chaque bulle de texte, en secondes (doit être supérieur d'au moins 2s à la durée)
+    const interval = 300;
 
+    // Liste des textes à afficher
+    const commands = [
+        'don',
+        'tshirt',
+        'album',
+        'eat',
+        'etc',
+    ];
+
+    const container = document.querySelector('#bot_command_container');
+    const text = document.querySelector('#bot_command');
+    let index = 0;
+
+    setInterval(() => {
+        index += 1;
+        if (index >= commands.length) {
+            index = 0;
+        }
+        text.innerText = `!${commands[index]}`;
+        container.classList.remove('translate-y-12');
+        setTimeout(() => {
+            container.classList.add('translate-y-12');
+        }, duration * 1000);
+    }, interval * 1000)
 </script>
 
 </body>
