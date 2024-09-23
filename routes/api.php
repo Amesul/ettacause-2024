fa806d9faeeb0b062daee55032ead6a889a3a232
Amesul <samuelstroesser@icloud.com>
2024-09-23 13:24:44 +0200

    Ajout d'un préfix aux routes nommées de l'API

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\StreamerController;
 use App\Http\Middleware\ApiMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api/v1')->middleware(ApiMiddleware::class)->group(function () {
+Route::prefix('api/v1')->name('api.')->middleware(ApiMiddleware::class)->group(function () {
     Route::apiResource('streamers', StreamerController::class, [
         'only' => ['index', 'show'],
     ]);
