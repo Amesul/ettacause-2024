96455f4f44a53d10d9d4862f149f300515ac0669
Amesul <samuelstroesser@icloud.com>
2024-09-23 17:58:59 +0200

    Ajout d'une route pour générer le HTML/CSS custom sans passer par SLC

--- resources/views/goals/create.blade.php
+++ resources/views/goals/create.blade.php
@@ -11,8 +11,8 @@
             <x-input-error :messages="$errors->get('charity-milestones')" class="mt-2"/>
         </div>
 
-        <div class="flex items-center justify-end mt-4 gap-4">
-            <p class="text-sm font-bold underline text-gray-600">Je n'ai pas de DG.</p>
+        <div class="flex items-baseline justify-end mt-4 gap-4">
+            <a href="{{ route('goals.show') }}" class="text-sm font-bold underline text-gray-600">Je n'ai pas de DG.</a>
             <x-secondary-button type="submit" class="ml-4">
                 {{ __('Générer') }}
             </x-secondary-button>
